95a560e84d9622535d30cece2442f26ea7e964f4
lubna40 <lubu7035@gmail.com>
2021-04-08 11:33:33 +0600

    Add next cnfig file

---
+++ frontend-nextjs/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    NEXT_PUBLIC_API_PATH: 'http://localhost:8081/api'
+  }
+}
--- frontend-nextjs/pages/api/api.js
+++ frontend-nextjs/pages/api/api.js
@@ -1,23 +1,24 @@
 import axios from "axios"
+const BASE_URL = process.env.NEXT_PUBLIC_API_PATH;
 
 export const getAllTaskList = () => {
-  return axios.get("http://localhost:8081/api/tasks")
+  return axios.get(BASE_URL + "/tasks")
 }
 
 export const getTaskDetails = (id) => {
-  return axios.get("http://localhost:8081/api/tasks" + id)
+  return axios.get(BASE_URL + "/tasks" + id)
 }
 
 export const newTask = (newTask) => {
-  return axios.post("http://localhost:8081/api/tasks/new", newTask)
+  return axios.post(BASE_URL + "/tasks/new", newTask)
 }
 
 export const editTask = (id) => {
-  return axios.put("http://localhost:8081/api/tasks/edit/" + id)
+  return axios.put(BASE_URL + "/tasks/edit/" + id)
 }
 
 export const deleteTask = (id) => {
-  return axios.delete("http://localhost:8081/api/tasks/" + id)
+  return axios.delete(BASE_URL + "/tasks/" + id)
 }
 
 
